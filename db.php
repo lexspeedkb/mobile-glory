84664930d2b6d0f79159e72a646c6d7b26482eea
Alex <lexspeedkb@gmail.com>
2019-04-29 00:08:25 +0300

    db

--- db.php
+++ db.php
@@ -5,10 +5,8 @@ class Db{
 	public static function getConnection(){
 		if($_SERVER['HTTP_HOST']=="mobile-glory"){
 			$mysqli = new mysqli('127.0.0.1', 'root', '', 'mobile_glory');
-		}elseif($_SERVER['HTTP_HOST']=="artmyrm.xyz"){
+		}elseif($_SERVER['HTTP_HOST']=="mobile-glory.xyz"){
 			$mysqli = new mysqli('mysql.hostinger.ru', 'u358382877_myrm', 'j$?:2TBJcu?jI4OlPA', 'u358382877_myrm');
-		}elseif($_SERVER['HTTP_HOST']=="myrm-lexspeedkb.c9users.io"){
-			$mysqli = new mysqli('localhost', 'lexspeedkb', '', 'c9');
 		}else{
 			$mysqli = new mysqli('127.0.0.1', 'root', '', 'mobile_glory');
 		}
