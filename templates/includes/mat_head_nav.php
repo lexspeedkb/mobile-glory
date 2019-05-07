378bf6207bee85b32c5369e604b4655c6247eb02
Alex <lexspeedkb@gmail.com>
2019-05-08 02:40:29 +0300

    set wallet accessed to users

--- templates/includes/mat_head_nav.php
+++ templates/includes/mat_head_nav.php
@@ -30,11 +30,11 @@
     <nav class="mdl-navigation">
       <a class="mdl-navigation__link" href="/list">Турниры</a>
       <a class="mdl-navigation__link" href="/aboutUs">О нас</a>
+        <a class="mdl-navigation__link" href="/myWallet">Кошелёк</a>
       <?php if ($data['MyAccount']['organizer']==1): ?>
         <a class="mdl-navigation__link" href="/organize">Организация</a>
       <?php endif ?>
       <?php if ($data['MyAccount']['admin']==1): ?>
-        <a class="mdl-navigation__link" href="/myWallet">Кошелёк</a>
         <a class="mdl-navigation__link" href="/admin/promoCodes">/Промо коды</a>
         <a class="mdl-navigation__link" href="/admin/users">/Пользователи</a>
         <a class="mdl-navigation__link" href="/admin/organizers">/Организаторы</a>
